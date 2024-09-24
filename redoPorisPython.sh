0f5ec810cfe4b7d1823faa55dc4388195333fc55
Txinto Vaz <txinto@elporis.com>
2024-09-24 16:29:30 +0100

    Updating pyPORIS and removing fork scripts

--- redoPorisPython.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Working directory clean
-# Set the clean environment variable
-export PORIS_CLEAN=1
-
-# Execute the doPorisPython.sh
-./doPorisPython.sh $1 || { echo 'doPorisPython.sh failed' ; exit 1; }
