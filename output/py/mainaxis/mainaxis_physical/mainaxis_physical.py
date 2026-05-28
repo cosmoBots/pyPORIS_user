206ca4b1b5e139948c279ef38ec3a5e7cc4d8652
Txinto Vaz <txinto@gmail.com>
2026-05-28 08:11:13 +0100

    updating to new pyPORIS

---
+++ output/py/mainaxis/mainaxis_physical/mainaxis_physical.py
@@ -0,0 +1,16 @@
+from mainaxisPORIS import *
+
+class mainaxis_physical(mainaxisPORIS):
+    # Go to ARCGengIIIPORIS.py, navigate to the ##### Action triggers ##### section
+    # which is normally at the bottom of the class, and copy here the methods 
+    # to start overriding them, in order to convert the virtual device into a physical one
+    # Once this class has any content, remove the pass clause
+
+    pass
+
+
+thismodel = mainaxis_physical(12)
+
+print("Let's test our model ",thismodel.getRoot().getName())
+print("Current mode is ",thismodel.getRoot().getSelectedMode().getName())
+
